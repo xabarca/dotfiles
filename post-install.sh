071129e1e674c8f7a3d38eaf0b1c638505a1e518
Xavier Abarca <xabarca@lap1405-00136.itdeusto.local>
2020-11-30 19:09:08 +0100

    added lots of things

--- post-install.sh
+++ post-install.sh
@@ -36,9 +36,27 @@ packages () {
 	echo "[$(date '+%Y-%m-%d %H:%M.%s')] default packages done" >> $LOG_FILE
 }
 
+# ----- default packages (void) --------
+packages_void () {
+	sudo xbps-install -Sy xbps
+	sudo xbps-install -Su
+	sudo xbps-install -Sy gcc make pkg-config libX11-devel libXft-devel libXinerama-devel
+	
+	sudo xbps-install -Sy git vim xorg xserver-xorg gcc make xdo
+	sudo xbps-install -Sy libx11-dev lifxft-dev libxinerama-dev 
+	sudo xbps-install -Sy libpango1.0-dev libx11-xcb-dev libxcb-xinerama0-dev 
+	sudo xbps-install -Sy libxcb-util0-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-cursor-dev
+	sudo xbps-install -Sy libxcb-icccm4-dev libxcb-ewmh-dev libxcb-shape0-dev
+	sudo xbps-install -Sy compton feh fonts-font-awesome curl vifm dunst libnotify-bin
+	#sudo apt install -y pcmanfm lxappearance mpv cmus
+	echo "[$(date '+%Y-%m-%d %H:%M.%s')] default packages done" >> $LOG_FILE
+}
+
 # ----- folders in HOME ---------
-homefolders () {
+basicfolders () {
 	mkdir ~/downloads ~/music ~/bin ~/pictures ~/pictures/walls ~/videos
+	sudo mkdir /opt/git
+	sudo chown $USER /opt/git
 	echo "[$(date '+%Y-%m-%d %H:%M.%S')] home folders created" >> $LOG_FILE
 }
 
@@ -55,16 +73,49 @@ gitrepos () {
 	#git clone https://github.com/baskerville/sxhkd	sudo mkdir /opt/git
 	#git clone https://git.suckless.org/dwm
 	#git clone https://bitbucket.org/natemaia/dk.git
-	sudo mkdir /opt/git
-	sudo chown $USER /opt/git
-	cd /opt/git
-	git clone https://github.com/LukeSmithxyz/st
-	git clone https://gitlab.com/protesilaos/lemonbar-xft.git
-	git clone https://github.com/baskerville/bspwm
-	git clone https://github.com/baskerville/sxhkd
-	git clone https://github.com/linuxdabbler/suckless /opt/git/suckless_linuxdabbler
-	git clone https://bitbucket.org/natemaia/dk.git
-	echo "[$(date '+%Y-%m-%d %H:%M.%S')] git repos cloned" >> $LOG_FILE
+	#https://github.com/ronasimi/bar
+	if [ -z $1 ]; then
+		# st - Luke Smith' suckless st
+		git clone https://github.com/LukeSmithxyz/st /opt/git/st
+		cd /opt/git/st
+		make
+		sudo ln -fs /opt/git/st/st /usr/local/bin
+		# pack of LinuxDabbler suckless utils (we want dmenu)
+		git clone https://github.com/linuxdabbler/suckless /opt/git/suckless_linuxdabbler
+		cd /opt/git/suckless_linuxdabbler/dmenu
+		make
+		sudo ln -fs /opt/git/suckless_linuxdabbler/dmenu/dmenu /usr/local/bin
+		sudo ln -fs /opt/git/suckless_linuxdabbler/dmenu/dmenu_path /usr/local/bin
+		sudo ln -fs /opt/git/suckless_linuxdabbler/dmenu/dmenu_run /usr/local/bin
+		sudo ln -fs /opt/git/suckless_linuxdabbler/dmenu/stest /usr/local/bin
+		# Protesilaos' compile of lemonbar with xft support
+		git clone https://gitlab.com/protesilaos/lemonbar-xft.git /opt/git/lemonbar-xft
+		cd /opt/git/lemonbar-xft
+		make
+		sudo ln -fs /opt/git/lemonbar-xft/lemonbar /usr/local/bin
+		# sxhkd
+		git clone https://github.com/baskerville/ /opt/git/sxhkd
+		cd /opt/git/sxhkd
+		make
+		sudo make install
+	fi
+	if [ "$1" = "bspwm" ]; then
+		git clone https://github.com/baskerville/bspwm /opt/git/bspwm
+		cd /opt/git/bspwm
+		make
+		sudo make install
+	elif [ "$1" = "dwm" ]; then
+		git clone https://git.suckless.org/dwm  /opt/git/dwm
+		cd /opt/git/dwm
+		make
+		sudo ln -fs /opt/git/dwm/dwm /usr/local/bin
+	elif [ "$1" = "dk" ]; then
+		git clone https://bitbucket.org/natemaia/dk.git /opt/git/dk
+		cd /opt/git/dk
+		make
+		sudo ln -fs /opt/git/dk/dk /usr/local/bin
+	fi
+	echo "[$(date '+%Y-%m-%d %H:%M.%S')] git repos cloned, compiled and installed ($1)" >> $LOG_FILE
 }
 
 # ----- git compiling + install -------------------
@@ -85,6 +136,7 @@ gitinstalls () {
 	cd /opt/git/sxhkd
 	make
 	sudo make install
+	
 	sudo ln -fs /opt/git/lemonbar-xft/lemonbar /usr/local/bin
 	sudo ln -fs /opt/git/st/st /usr/local/bin
 	sudo ln -fs /opt/git/suckless_linuxdabbler/dwm/dwm /usr/local/bin
@@ -117,16 +169,65 @@ defaultbspwm () {
 	cp $ACTUAL_DIR/scratchpad.sh ~/bin
 	chmod +x ~/bin/scratchpad.sh
 	echo " " >> ~/.config/sxhkd/sxhkdrc
-	echo "super + ntilde" >> ~/.config/sxhkd/sxhkdrc
+	echo "super + a" >> ~/.config/sxhkd/sxhkdrc
     echo "    /home/$USER/bin/scratchpad.sh" >> ~/.config/sxhkd/sxhkdrc
 	echo "[$(date '+%Y-%m-%d %H:%M.%S')] bspwm & sxhkd installed and configured" >> $LOG_FILE
 }
 
 # ----- configure dwm -----------------------------
 configdwm () {
-	cp $ACTUAL_DIR/dwn* ~/bin
+	cp $ACTUAL_DIR/dwm* ~/bin
 	echo "dwm-start" >> .xinitrc
 	echo "[$(date '+%Y-%m-%d %H:%M.%S')] dwm configured" >> $LOG_FILE
+#
+#  ==== patching guide + plus ====
+# 
+# -- ::notes:: ----
+# git branch config       -- this makes a new branch
+# git branch              -- list existing branches
+# git checkout config     -- changes to this branch
+# git branch -D config    -- deletes non-active config branch
+# make clean && rm -f config.h && git reset -hard origin/master  -- goes to initial state (no make done), throwing away any potential not commited change
+# git merge config -m config  -- merge the contents of config branch into actual branch 
+#
+# -- starting ---
+# git branch config
+# git checkout config
+# change wethever you want (example: Mod1 -> Mod4)	
+# git add config.def.h
+# git commit -m config   -- author note (easy way): name of the commentary = name of branch
+# git checkout master
+# git merge config -m config
+#	.. and now we can test if the changes are OK (make & restarting dwm)
+#
+# -- let's patch pertag: first we want a clean original code again ---
+# git checkout master
+# make clean && rm -f config.h && git reset -hard origin/master
+# git branch pertag
+# git checkout pertag
+# git apply /location/of/pertag-patch.diff	
+#   .. we can see if the changes are applied in the code ...	
+# git add .	
+# git commit -m pertag	
+# git checkout master	
+# git merge config -m config	
+# git merge pertag -m pertag	-- we merged all changes we did (one by one)
+#  ... now we can make and see if the canghes are OK ...	
+# 
+# -- let's noborder now ---
+# git checkout master
+# make clean && rm -f config.h && git reset -hard origin/master
+# git branch noborder
+# git checkout noborder
+# git apply /location/of/noborder-patch.diff	
+#   .. we can see if the changes are applied in the code ...	
+# git add .	
+# git commit -m noborder	
+# git checkout master	
+# git merge config -m config	
+# git merge pertag -m pertag
+# git merge noborder -m noborder
+#	
 }
 
 # ----- configure dk -----------------------------
@@ -134,10 +235,11 @@ configdk () {
 	mkdir ~/.config/dk
 	cp /opt/git/dk/doc/scripts/bar.sh ~/bin/bar-dk.sh
 	cp /opt/git/dk/doc/dkrc ~/.config/dk/
-	cp /opt/git/dk/doc/dkrc/sxhkdrc ~/.config/sxhkd/sxhkdrc.dk
+	cp /opt/git/dk/doc/sxhkdrc ~/.config/sxhkd/sxhkdrc.dk
 	chmod +x ~/.config/dk/dkrc
 	chmod +x ~/.config/sxhkd/sxhkdrc.dk
-	sed -i 's/sxhkd &/sxhkd -x ~\/\.config\/sxhkd\/sxhkdrc\.dk/' ~/.config/dk/dkrc
+	chmod +x ~/bin/dk-bar.sh
+	sed -i 's/sxhkd &/sxhkd -c ~\/\.config\/sxhkd\/sxhkdrc\.dk/' ~/.config/dk/dkrc
 	sed -i 's/alt/super/' ~/.config/sxhkd/sxhkdrc.dk
 	sed -i 's/exit 0/compton &/' ~/.config/dk/dkrc
 	echo "dunst &" >> ~/.config/dk/dkrc
@@ -147,7 +249,7 @@ configdk () {
 }
 
 # ----- lemonbar panel -----------------------------
-lemonbarpanel () {
+lemonbarpanelbsp () {
 	cp $ACTUAL_DIR/panel* ~/bin
 	cp $ACTUAL_DIR/launch-bar ~/bin
 	chmod +x ~/bin/*
@@ -168,38 +270,40 @@ walls () {
 	curl -O http://static.simpledesktops.com/uploads/desktops/2015/03/02/mountains-on-mars.png
 	curl -O http://static.simpledesktops.com/uploads/desktops/2015/02/20/zentree_1.png
 	curl -O http://static.simpledesktops.com/uploads/desktops/2013/09/18/wallpaper.png
-	[ -z ~/.config/bspwm/bspwmrc ] && echo "wallpaper-loop &" >> ~/.config/bspwm/bspwmrc
-	[ -z ~/.config/dk/dkrc ]; then
+	[ -f ~/.config/bspwm/bspwmrc ] && echo "wallpaper-loop &" >> ~/.config/bspwm/bspwmrc
+	if [ -f ~/.config/dk/dkrc ]; then
 		sed -i 's/exit 0/wallpaper-loop &/' ~/.config/dk/dkrc
 		echo "exit 0" >> ~/.config/dk/dkrc
 	fi
-	echo "[$(date '+%Y-%m-%d %H:%M.%S')] wallpapers downloaded" >> $LOG_FILE
+	echo "[$(date '+%Y-%m-%d %H:%M.%S')] wallpapers downloaded and working" >> $LOG_FILE
 }
 
 # ----- xinit & bashrc ----------------------------
 finalsetup () {
 	sed -i 's/#alias ll=/alias ll=/' ~/.bashrc
+	cp $ACTUAL_DIR/choosewm ~/bin
+	chmod +x ~/bin/choosewm
 	if [ "$WM_SELECTION" = "dwm" ]; then
-		echo "dwm-start" >> .xinitrc
+		echo "dwm-start" >> ~/.xinitrc
 	elif [ "$WM_SELECTION" = "bspwm" ]; then
-		echo "exec bspwm" >> .xinitrc 
+		echo "exec bspwm" >> ~/.xinitrc
 	elif [ "$WM_SELECTION" = "dk" ]; then
 		echo "~/bin/bar-dk.sh &" >> .xinitrc 
-		echo "exec dk" >> .xinitrc 
+		echo "exec dk" >> ~/.xinitrc 
 	elif [ "$WM_SELECTION" = "all" ]; then
-		echo "if [ -f $WM_SELECTION_FILE ]; then" >> .xinitrc
-		echo "   SEL=\$(cat $WM_SELECTION_FILE)" >> .xinitrc
-		echo "   if [ \$SEL = dwm ]; then"  >> .xinitrc
-		echo "      dwm-start" >> .xinitrc
-		echo "   elif [ \$SEL = dk ]; then" >> .xinitrc
-		echo "      ~/bin/bar-dk.sh &" >> .xinitrc 
-		echo "      exec dk" >> .xinitrc 
-		echo "   else" >> .xinitrc
-		echo "      exec bspwm" >> .xinitrc
-		echo "   fi"  >> .xinitrc
-		echo "else"  >> .xinitrc
-		echo "   exec bspwm"  >> .xinitrc
-		echo "fi" >> .xinitrc
+		echo "if [ -f $WM_SELECTION_FILE ]; then" >> ~/.xinitrc
+		echo "   SEL=\$(cat $WM_SELECTION_FILE)" >> ~/.xinitrc
+		echo "   if [ \$SEL = dwm ]; then"  >> ~/.xinitrc
+		echo "      dwm-start" >> ~/.xinitrc
+		echo "   elif [ \$SEL = dk ]; then" >> ~/.xinitrc
+		echo "      ~/bin/bar-dk.sh &" >> ~/.xinitrc
+		echo "      exec dk" >> ~/.xinitrc
+		echo "   else" >> ~/.xinitrc
+		echo "      exec bspwm" >> ~/.xinitrc
+		echo "   fi"  >> ~/.xinitrc
+		echo "else"  >> ~/.xinitrc
+		echo "   exec bspwm"  >> ~/.xinitrc
+		echo "fi" >> ~/.xinitrc
 	fi
 	echo "[$(date '+%Y-%m-%d %H:%M.%S')] xinitrc & bash alias setups done" >> $LOG_FILE
 }
@@ -216,19 +320,19 @@ finalsetup () {
 
 if [ "$OPTION" = "0" ]; then
 	WM_SELECTION="all"
-	packages && homefolders && gitrepos && gitinstalls && defaultbspwm && lemonbarpanel && configdwm && configdk && walls && finalsetup && echo "bspwm & dwm configured. Please, reboot system."
+	packages && basicfolders && gitrepos && gitrepos bspwm && gitrepos dwm && gitrepos dk && defaultbspwm && lemonbarpanelbsp && configdwm && configdk && walls && finalsetup && echo "bspwm & dwm & dk configured. Please, reboot system."
 	echo "bspwm" > $WM_SELECTION_FILE
 elif [ "$OPTION" = "1" ]; then
 	WM_SELECTION="bspwm"
-	packages && homefolders && gitrepos && gitinstalls && defaultbspwm && lemonbarpanel && walls && finalsetup && echo "bspwm configured. Please, reboot system."
+	packages && basicfolders && gitrepos && gitrepos bspwm && defaultbspwm && lemonbarpanelbsp && walls && finalsetup && echo "bspwm configured. Please, reboot system."
 	echo "bspwm" > $WM_SELECTION_FILE
 elif [ "$OPTION" = "2" ]; then
 	WM_SELECTION="dwm"
-	packages && homefolders && gitrepos && gitinstalls && configdwm && walls && finalsetup && echo "dwm configured. Please, reboot system."
+	packages && basicfolders && gitrepos && gitrepos dwm && configdwm && walls && finalsetup && echo "dwm configured. Please, reboot system."
 	echo "dwm" > $WM_SELECTION_FILE
 elif [ "$OPTION" = "3" ]; then
 	WM_SELECTION="dk"
-	packages && homefolders && gitrepos && gitinstalls && configdk && walls && finalsetup && echo "dk configured. Please, reboot system."
+	packages && basicfolders && gitrepos && gitrepos dk && configdk && walls && finalsetup && echo "dk configured. Please, reboot system."
 	echo "dk" > $WM_SELECTION_FILE
 else
 	usage
